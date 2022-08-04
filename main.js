c50e948be15cacae19d30e090087df3f2ded2db8
swiftspyslash123 <81601696+swiftspyslash123@users.noreply.github.com>
2022-08-03 21:04:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ rightWristY = 0;
 leftWristX = 0;
 leftWristY = 0;
 
+scoreLeftWrist = 0;
+
 function preload(){
-    song1 = loadsound("music.mp3");
-    song2 = loadsound("music2.mps");
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function setup(){
@@ -33,15 +35,17 @@ function gotPoses()
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;  
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-        leftWristX = results[0].pose.rightWrist.x;
-        leftWristY = results[0].pose.rightWrist.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY  = "+ leftWristY);
     }
 }
@@ -51,6 +55,41 @@ function draw(){
 
     fill("#A020F0");
     stroke("#A020F0");
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+
+        if(rightWristY >0 && rightWristY <=100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+    
+        else if(rightWristY >100 && rightWristY <= 200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+    
+        else if(rightWristY >200 && rightWristY <= 300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+    
+        else if(rightWristY >300 && rightWristY <= 400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+    
+        else if(rightWristY >400 && rightWristY <= 500)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
 
     if(scoreLeftWrist > 0.2)
     {
